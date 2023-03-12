5601aa7a405648b06748411fa68f6b9ee7df6b6d
Maicol <mamian642@gmail.com>
2023-03-12 15:57:31 -0500

    Maquetizacion del Login y Logo
    -Hice todo el html y css(Con bootstrap) del login
    -Hice otro logo donde es mas pequeño y no tiene fondo
    -Cree 3 cosas:
    Logo
    Login.php
    style.css
    Todos en la carpeta Login
    TODO esta sujeto a cambios ya que se tiene que hablar en reunion

---
+++ SoxStyle_WEB/vista/Login/login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <!--BARRA DE NAVEGACION-->
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="../../images/LogoSinfondo.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+      Sox Style<!--Texto al lado de la foto-->
+    </a>
+    <span class="navbar-text">
+        Usuario <!--Texto al final de la barra-->
+      </span>
+  </div>
+</nav>
+<!--FIN BARRA DE NAVEGACION-->
+<br>
+<div class="contenedor">
+<img src="../../images/LogoSinfondo.png">
+
+<form>
+    <br>
+    <br>
+  <div class="row mb-3">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Correo</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3">
+    </div>
+  </div>
+  <div class="row mb-3">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Contraseña</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3">
+    </div>
+  </div>
+  <div class="row mb-3">
+    <div class="col-sm-10 offset-sm-2">
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" id="gridCheck1">
+        <label class="form-check-label" for="gridCheck1">
+          Puede servir para algo?
+        </label>
+      </div>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Ingresar</button>
+</form>
+</div>
+</body>
+</html>
