a84f9258296a234ffaf827dc156173ecec38bb6c
VictorPerlaza <victorperlaza11b@gmail.com>
2023-03-12 13:02:56 -0500

    Cree las vistas de la pagina de compra y lista de productos
    Esta es la maquetación de la pagina de compras y lista de productos

---
+++ SoxStyle_WEB/vista/lista_de_productos.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de productos</title>
+</head>
+<body>
+    <h1>Lista de productos</h1>
+    <table> 
+        <tr>
+        <th>Camisa verde</th>
+        <td>$********</td>
+        </tr>
+        <tr>
+        <th>Camisa roja</th>
+        <td>$*******</td>
+        </tr>
+        <tr>
+        <th>Camisa negra</th>
+        <td>$*******</td>
+        </tr>
+        <tr>
+        <th>Total</th>
+        <td>$*******</td>
+        </tr>
+    </table>
+    <br>
+    <br>
+    <form action="">
+        <label for="Texto contrasena">Por favor ingrese la contraseña para realizar la compra</label>
+
+        <label for="contrasena">Contraseña</label>
+        <input type="text" name="contrasena">
+        <br>
+        <br>
+        <button type="submit">Comprar</button>
+
+    </form>
+</body>
+</html>
