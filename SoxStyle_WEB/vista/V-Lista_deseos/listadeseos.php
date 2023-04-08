376b726af20c59dcf0fff484d5df957f2ec74a2d
Juanmanuelx24 <juanmanuelx242@gmail.com>
2023-04-07 19:51:26 -0500

    ListadeDeseos
    Desplazamiento a carrito de compras y pagina principal

--- SoxStyle_WEB/vista/V-Lista_deseos/listadeseos.php
+++ SoxStyle_WEB/vista/V-Lista_deseos/listadeseos.php
@@ -54,39 +54,40 @@
                     <path d="M17 17h-11v-14h-2" />
                     <path d="M6 5l14 1l-1 7h-13" />
                     </svg>
-                    <a href="../carrito de compras/"></a><img src="/E_Commerce_Web/SoxStyle_WEB/icons/Lg-cancelar.png alt="">
-                </div>
-
-                <div class="columna">
-                    <h2>Nombre del producto</h2>
-                    <p>$ XXXXXXX</p>
-                    <button class="button-9" role="button">Quitar</button>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <circle cx="6" cy="19" r="2" />
-                    <circle cx="17" cy="19" r="2" />
-                    <path d="M17 17h-11v-14h-2" />
-                    <path d="M6 5l14 1l-1 7h-13" />
-                    </svg>
-                    <img src="/E_Commerce_Web/SoxStyle_WEB/icons/Lg-cancelar.png alt="">
-                </div>
-
-                <div class="columna">
-                    <h2>Nombre del producto</h2>
-                    <p>$ XXXXXXX</p>
-                    <button class="button-9" role="button">Quitar</button>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <circle cx="6" cy="19" r="2" />
-                    <circle cx="17" cy="19" r="2" />
-                    <path d="M17 17h-11v-14h-2" />
-                    <path d="M6 5l14 1l-1 7h-13" />
-                    </svg>
-                    <a href="../carrito de compras/CarritoCompras.php">.</a>
-                    <img src="/icons/Lg-añadido-fav.png">
                     
+                    <a href="../carrito de compras/CarritoCompras.php">Carrito de compras</a>
+                </div>
+
+                <div class="columna">
+                    <h2>Nombre del producto</h2>
+                    <p>$ XXXXXXX</p>
+                    <button class="button-9" role="button">Quitar</button>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <circle cx="6" cy="19" r="2" />
+                    <circle cx="17" cy="19" r="2" />
+                    <path d="M17 17h-11v-14h-2" />
+                    <path d="M6 5l14 1l-1 7h-13" />
+                    </svg>
+                    
+                    <a href="../carrito de compras/CarritoCompras.php">Carrito de compras</a>
+                </div>
+
+                <div class="columna">
+                    <h2>Nombre del producto</h2>
+                    <p>$ XXXXXXX</p>
+                    <button class="button-9" role="button">Quitar</button>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <circle cx="6" cy="19" r="2" />
+                    <circle cx="17" cy="19" r="2" />
+                    <path d="M17 17h-11v-14h-2" />
+                    <path d="M6 5l14 1l-1 7h-13" />
+                    </svg>
+                    <a href="../carrito de compras/CarritoCompras.php">Carrito de compras</a>
                 </div>
             </div>
+            <a href="../Principal_producto/principal_p.html">Principal</a>
             <br>
             <br>
         <hr>
